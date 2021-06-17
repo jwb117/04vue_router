353caaa237c2860970cce1809aea7989ff0d127b
jwb117 <jwbjoo@naver.com>
2021-06-17 11:17:32 +0900

    mon2

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-      <h1>logo</h1>
-      <nav>
-          <ul class="gnb clearfix">
-              <li v-for="(jwb, i) in menu" :key="i">
-                <router-link :to="menu[i].link">{{menu[i].title}}</router-link>
-              </li>
-          </ul>
-      </nav>
+      <h1 class=" text-center">logo</h1>
+      <div class="container">
+          
+              <nav>
+                  <ul class="gnb clearfix">
+                      <li v-for="(jwb, i) in menu" :key="i" >
+                        <router-link :to="menu[i].link">{{menu[i].title}}</router-link>
+                      </li>
+                  </ul>
+              </nav>
+          
+      </div>
   </div>
 </template>
 
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,13 @@
 <template>
   <h2>home</h2>
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-md-3 bg-info">1</div>
+      <div class="col-md-3">2</div>
+      <div class="col-md-3 bg-primary">3</div>
+      <div class="col-md-3">4</div>
+    </div>
+  </div>
 </template>
 
 <script>
