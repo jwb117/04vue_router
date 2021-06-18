617693b41022e77154ef0e432f5477122af55ce5
jwb117 <jwbjoo@naver.com>
2021-06-18 10:05:07 +0900

    morning

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>      
       <div class="container">    
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
             <a class="navbar-brand" href="/">LOGO</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
--- src/components/product.vue
+++ src/components/product.vue
@@ -1,12 +1,12 @@
 <template>
-  <h2>TOUR</h2>
+  <h2 class="text-center">TOUR</h2>
 
 <div class="best">
     <div class="container">
       <h2 class="text-center">싱가포르 여행 개별 추천상품</h2>
       <p>가장 인기많은 싱가포르 베스트 여행 상품을 소개합니다.</p>
         <div class="row">
-            <div class="col-md-4 col-sm-6" v-for="(jwb, i) in listData" :key="i">
+            <div class="col-md-4 col-sm-6 text-center" v-for="(jwb, i) in listData" :key="i">
                 <router-link :to="`/detail/${listData[i].id}`">
                     <img :src="listData[i].image" alt="" class="w-100">
                     {{listData[i].title}}
