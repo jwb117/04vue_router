4fd8455d53ed57c00f6358399f8433438af8e490
jwb117 <jwbjoo@naver.com>
2021-06-24 11:44:09 +0900

    ff

--- src/components/company.vue
+++ src/components/company.vue
@@ -2,7 +2,7 @@
   <h2>COMPANY</h2>
     <p>찾아 오시는 길</p>
   <div class="container">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.2055548356184!2d126.87679691516585!3d37.50306977980986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9fc70bce5993%3A0xdd5b5b33e55b538e!2z7ZWY7J2066-465SU7Ja07Lu07ZOo7YSw7ZWZ7JuQ6rWs66Gc7Lqg7Y287Iqk!5e0!3m2!1sko!2skr!4v1624501907053!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.2055548356184!2d126.87679691516585!3d37.50306977980986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9fc70bce5993%3A0xdd5b5b33e55b538e!2z7ZWY7J2066-465SU7Ja07Lu07ZOo7YSw7ZWZ7JuQ6rWs66Gc7Lqg7Y287Iqk!5e0!3m2!1sko!2skr!4v1624501907053!5m2!1sko!2skr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy">
   </iframe>
   </div>
     <h2>회사소개</h2>
