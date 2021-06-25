238817924867bbe5ea0a23b001695a6a7a9e6e07
jwb117 <jwbjoo@gmail.com>
2021-06-25 18:10:57 +0900

    dd

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ module.exports = {
       name: 'SingaPore Tour',
       themeColor: '#A566FF',
       msTileColor: '#000000',
+      manifestOptions: {
+        background_color: "#ffffff"
+      },
       workboxOptions: {
         exclude: [/\.map$/, /manifest\.json$/, 'index.html']
       }
